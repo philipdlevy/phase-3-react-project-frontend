08d68c28ab91ea0e592321da136afc093f39186b
Philip Levy <philipdlevy@gmail.com>
2022-06-20 12:50:17 -0600

    added buttons and styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,9 +4,9 @@ function HomePage() {
   return (
     <div className="bg">
       <h1 className='welcome'>Welcome!</h1>
-        <button>HomePage</button>
-        <button>HomePage</button>
-        <button>HomePage</button>
+        <button className='button1'>HomePage</button>
+        <button className='button2'>Add Book</button>
+        <button className='button3'>Book List</button>
     </div>
   );
 }
